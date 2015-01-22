f06f448af6d4bcc41fa621b89d0da81dd945bbd7
evg31337 <evg31337@gmail.com>
2015-01-22 17:10:12 -0500

    WHY DIRS

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', __dirname + '/');
 app.use(express.static(__dirname + "/public"));
 //app.use('/js', express.static(__dirname + '/js'));
 //app.use('/css', express.static(__dirname + '/css'));
