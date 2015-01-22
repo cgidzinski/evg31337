664af82dce382d7ba8b1241f00952fb96a6f0914
evg31337 <evg31337@gmail.com>
2015-01-22 16:51:22 -0500

    Pushed root index dir

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(__dirname + "/public"));
 //app.use('/js', express.static(__dirname + '/js'));
 //app.use('/css', express.static(__dirname + '/css'));
 //app.use('/partials', express.static(__dirname + '/partials'));
