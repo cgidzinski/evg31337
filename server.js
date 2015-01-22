f65e4f22977008fe90951ff018a2bf0585ce0c9d
evg31337 <evg31337@gmail.com>
2015-01-22 16:46:31 -0500

    Updated js server and app files

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/partials', express.static(__dirname + '/partials'));
+//app.use('/js', express.static(__dirname + '/js'));
+//app.use('/css', express.static(__dirname + '/css'));
+//app.use('/partials', express.static(__dirname + '/partials'));
 
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendfile('index.html', { root: __dirname });
-});
+//app.all('/*', function(req, res, next) {
+  //  // Just send the index.html for other files to support HTML5Mode
+ //   res.sendfile('index.html', { root: __dirname });
+//});
 
 
 app.listen(app.get('port'), function() {
