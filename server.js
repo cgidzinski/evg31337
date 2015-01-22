b37ec6c35e414d4ce3a4506e87e8acebd802f31a
evg31337 <evg31337@gmail.com>
2015-01-22 16:55:09 -0500

    maybe now dir works

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 //app.use('/js', express.static(__dirname + '/js'));
 //app.use('/css', express.static(__dirname + '/css'));
-//app.use('/partials', express.static(__dirname + '/partials'));
+app.use('/partials', express.static(__dirname + '/public/partials'));
 
 //app.all('/*', function(req, res, next) {
   //  // Just send the index.html for other files to support HTML5Mode
