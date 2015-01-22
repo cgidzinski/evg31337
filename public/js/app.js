b6f8c44c214da3a7d9089ec589c70e9fecdbeea0
evg31337 <evg31337@gmail.com>
2015-01-22 17:00:55 -0500

    Renamed partials to views

--- public/js/app.js
+++ public/js/app.js
@@ -8,42 +8,42 @@ myApp.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
   //}
   // For any unmatched url, redirect to /state1
   //$urlRouterProvider.when('', '/');
-  //$urlRouterProvider.otherwise("/partials/404");
+  //$urlRouterProvider.otherwise("/views/404");
  // $urlRouterProvider.otherwise('404');
   //
   // Now set up the states
   $stateProvider
     .state('home', {
       url: "/",
-      templateUrl: "partials/home.html"
+      templateUrl: "views/home.html"
     })
     .state('hardware', {
       url: "/hardware",
-      templateUrl: "partials/project_list.html"
+      templateUrl: "views/project_list.html"
     })
     .state('software', {
       url: "/software",
-      templateUrl: "partials/project_list.html"
+      templateUrl: "views/project_list.html"
     })
  .state('interests', {
       url: "/interests",
-      templateUrl: "partials/interests.html"
+      templateUrl: "views/interests.html"
     })  
     .state('about', {
       url: "/about",
-      templateUrl: "partials/about.html",
+      templateUrl: "views/about.html",
     })
     .state('contact', {
       url: "/contact",
-      templateUrl: "partials/contact.html"
+      templateUrl: "views/contact.html"
     })
     .state('misc', {
       url: "/misc",
-      templateUrl: "partials/misc.html",
+      templateUrl: "views/misc.html",
       controller: "BlogCtrl"
     })
    .state('404', {
       url: "/404",
-      templateUrl: 'partials/404.html'
+      templateUrl: 'views/404.html'
     });
 });
