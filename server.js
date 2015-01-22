4d49532bd448aa50a776beae43c31fa4da71e3b2
evg31337 <evg31337@gmail.com>
2015-01-22 16:54:09 -0500

    more dir

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/css', express.static(__dirname + '/css'));
+//app.use('/js', express.static(__dirname + '/js'));
+//app.use('/css', express.static(__dirname + '/css'));
 //app.use('/partials', express.static(__dirname + '/partials'));
 
 //app.all('/*', function(req, res, next) {
