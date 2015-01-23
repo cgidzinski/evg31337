a02c09e3bb0838a5f2c31fb7350eb7a624d67d6b
evg31337 <evg31337@gmail.com>
2015-01-23 15:33:33 -0500

    initial mongo schema

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
-
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -14,17 +13,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+var projectSchema = new mongoose.Schema({
+  _id: Number,
+  title: String,
+  date: Date,
+  titleSubtext: String,
+  image: String,
+  quickDescription: String,
+  mainText: String,
+  poster: String,
+  links: [{
+      text: String,
+      url: String
+  }]
+});
+var Project = mongoose.model('Project', projectSchema);
+
+mongoose.connect('localhost');
+
+
+
+
+
+
+
+
+
 app.set('views', __dirname + '/');
 app.use(express.static(__dirname + "/public"));
-
 app.use('/*', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
-
-//app.config(function($resourceProvider) {
- // $resourceProvider.defaults.stripTrailingSlashes = true;
-//});
-
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
