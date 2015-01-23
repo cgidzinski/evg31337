9d635162f086fb5b2f780454101e58af234bfb5a
evg31337 <evg31337@gmail.com>
2015-01-22 23:50:29 -0500

    html5 fix

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ app.use(express.static(__dirname + "/public"));
 //app.use('/css', express.static(__dirname + '/css'));
 //app.use('/partials', express.static(__dirname + '/public/partials'));
 
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendfile('index.html', { root: __dirname });
+app.use(function(req, res) {
+    res.sendfile(__dirname + '/Public/index.html');
 });
 
 
