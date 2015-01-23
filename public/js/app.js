697f035c6f0d15fff0c8b3a68af8a5280274b003
evg31337 <evg31337@gmail.com>
2015-01-23 00:42:13 -0500

    strip slash url

--- public/js/app.js
+++ public/js/app.js
@@ -43,4 +43,20 @@ myApp.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
       url: "/404",
       templateUrl: 'views/404.html'
     });
+
+
+  $urlRouterProvider.rule(function($injector, $location) {
+
+    var path = $location.path();
+    var hasTrailingSlash = path[path.length-1] === '/';
+
+    if(hasTrailingSlash) {
+
+      //if last charcter is a slash, return the same url without the slash  
+      var newPath = path.substr(0, path.length - 1); 
+      return newPath; 
+    } 
+
+  });
+   
 });
