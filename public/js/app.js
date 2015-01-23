2452b0aca1842738990e873ecf61aff4c935793d
evg31337 <evg31337@gmail.com>
2015-01-23 01:09:46 -0500

    WORKING BASE WITH HTML5 REDIRECTS
    THIS IS GOLD

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,8 @@ var myApp = angular.module('evg31337', ['ui.router']);
 
 myApp.config(function($stateProvider, $urlRouterProvider,$locationProvider,$urlMatcherFactoryProvider) {
   //
-  //if(window.history && window.history.pushState){
-  // $locationProvider.html5Mode(true)
-  //}
+ $locationProvider.html5Mode(true)
+
  $urlMatcherFactoryProvider.strictMode(false)
  // $urlRouterProvider.when('', '/');
  $urlRouterProvider.otherwise('404');
