37ced66ad7c2aae44c81e8e4b638b9f8adfa3679
evg31337 <evg31337@gmail.com>
2015-01-23 02:00:39 -0500

    Minor Tweaks

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,44 @@
 var myApp = angular.module('evg31337', ['ui.router']);
 
-myApp.config(function($stateProvider, $urlRouterProvider,$locationProvider,$urlMatcherFactoryProvider) {
-  //
- $locationProvider.html5Mode(true)
+myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider, $urlMatcherFactoryProvider) {
+    //
+    $locationProvider.html5Mode(true)
 
- $urlMatcherFactoryProvider.strictMode(false)
- // $urlRouterProvider.when('', '/');
- $urlRouterProvider.otherwise('404');
-  // Now set up the states
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "/views/home.html"
-    })
-    .state('hardware', {
-      url: "/hardware",
-      templateUrl: "views/project_list.html"
-    })
-    .state('software', {
-      url: "/software",
-      templateUrl: "views/project_list.html"
-    })
- .state('interests', {
-      url: "/interests",
-      templateUrl: "views/interests.html"
-    })  
-    .state('about', {
-      url: "/about",
-      templateUrl: "views/about.html",
-    })
-    .state('contact', {
-      url: "/contact",
-      templateUrl: "views/contact.html"
-    })
-    .state('misc', {
-      url: "/misc",
-      templateUrl: "views/misc.html",
-      controller: "BlogCtrl"
-    })
-   .state('404', {
-      url: "/404",
-      templateUrl: 'views/404.html'
-    });
-});  
-
-
-   
+    $urlMatcherFactoryProvider.strictMode(false)
+        // $urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise('404');
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "/views/home.html"
+        })
+        .state('hardware', {
+            url: "/hardware",
+            templateUrl: "views/project_list.html"
+        })
+        .state('software', {
+            url: "/software",
+            templateUrl: "views/project_list.html"
+        })
+        .state('interests', {
+            url: "/interests",
+            templateUrl: "views/interests.html"
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "views/about.html",
+        })
+        .state('contact', {
+            url: "/contact",
+            templateUrl: "views/contact.html"
+        })
+        .state('misc', {
+            url: "/misc",
+            templateUrl: "views/misc.html"
+        })
+        .state('404', {
+            url: "/404",
+            templateUrl: 'views/404.html'
+        });
+});
--- public/js/controllers.jss
+++
@@ -1,5 +0,0 @@
-myApp.controller('BlogCtrl', function($scope, $http, $stateParams, $location) {
-//Get variables from previous page routed parameters
-   console.log("Blog Controller reporting for duty.");
-
-})
