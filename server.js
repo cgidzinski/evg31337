eda76d838cf09bc237978faaab57b44596f8ca2b
evg31337 <evg31337@gmail.com>
2015-02-03 20:02:11 -0500

    cors test

--- server.js
+++ server.js
@@ -78,7 +78,11 @@ var About = mongoose.model('About', aboutSchema);
 mongoose.connect('localhost');
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('views', __dirname + '/');
 app.use(express.static(__dirname + "/public"));
